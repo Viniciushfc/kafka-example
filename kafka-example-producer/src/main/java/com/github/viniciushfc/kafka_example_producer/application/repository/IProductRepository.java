aab068463916da4a766fad047d98078a3f90c145
ViniciusHFC <vinicius.henry.fc@gmail.com>
2026-03-13 00:40:07 -0300

    Implementa dependencia do jackson databind, troca de @Query e config para iniciar o data.sql junto com a aplicação

--- kafka-example-producer/src/main/java/com/github/viniciushfc/kafka_example_producer/application/repository/IProductRepository.java
+++ kafka-example-producer/src/main/java/com/github/viniciushfc/kafka_example_producer/application/repository/IProductRepository.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 @Repository
 public interface IProductRepository extends JpaRepository<Product, UUID> {
 
-    @Query(value = "SELECT p.id FROM product p", nativeQuery = true)
+    @Query("SELECT p.id FROM Product p")
     List<UUID> findAllIds();
 
 }
---
+++ kafka-example-producer/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO product (id, name, quantity, price)
+VALUES (RANDOM_UUID(), 'Produto Exemplo', 10, 49.90);
